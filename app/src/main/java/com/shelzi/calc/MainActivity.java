fc2fdb159ab1d4c9928fc8cbed1a45f0fe2a0c24
Shelzi <nemant38@gmail.com>
2021-09-10 00:01:11 +0300

    Fixed result button

--- app/src/main/java/com/shelzi/calc/MainActivity.java
+++ app/src/main/java/com/shelzi/calc/MainActivity.java
@@ -2,19 +2,32 @@ package com.shelzi.calc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
+import com.udojava.evalex.Expression;
+
+public class MainActivity extends AppCompatActivity {
 
     TextView tvOut;
     TextView tvIn;
     Button btnOne;
     Button btnTwo;
+    Button btnThree;
+    Button btnFore;
+    Button btnFive;
+    Button btnSix;
+    Button btnSeven;
+    Button btnEight;
+    Button btnNine;
+    Button btnZero;
+    Button btnResult;
     TextView myAwesomeTextView;
 
     @Override
@@ -24,41 +37,91 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnOne = (Button) findViewById(R.id.btnOne);
         btnTwo = (Button) findViewById(R.id.btnTwo);
+        btnThree = (Button) findViewById(R.id.btnThree);
+        btnFore = (Button) findViewById(R.id.btnFore);
+        btnFive = (Button) findViewById(R.id.btnFive);
+        btnSix = (Button) findViewById(R.id.btnSix);
+        btnSeven = (Button) findViewById(R.id.btnSeven);
+        btnEight = (Button) findViewById(R.id.btnEight);
+        btnNine = (Button) findViewById(R.id.btnNine);
+        btnZero = (Button) findViewById(R.id.btnZero);
+
+        btnResult = (Button) findViewById(R.id.btnResult);
         tvIn = (TextView)findViewById(R.id.tvIn);
         tvOut = (TextView)findViewById(R.id.tvOut);
+        tvOut.setText("Test"); // Это работает, текст изменяется
+        getSupportActionBar().hide();
 
-        btnOne.setOnClickListener(this);
-        btnTwo.setOnClickListener(this);
+        View.OnClickListener oclBtnResult = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Expression expression = new Expression((String)tvIn.getText());
+                String answer = new  String(String.valueOf(expression.eval()));
+                tvOut.setText(answer);
+            }
+        };
 
+        btnResult.setOnClickListener(oclBtnResult);
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.btnOne:
-                String tempString = new String("");
-                if (tvIn.getText() != "")
-                    tempString = (String) tvIn.getText();
-                tvIn.setText(tempString + "1");
 
-                break;
 
-            case R.id.btnTwo:
-                tvIn.setText("2");
-                break;
-
-            case R.id.btnResult:
-/*                Expression expression = new Expression("1+1/3");
-                tvOut.setText(expression.toString());*/
-        }
-    }
 
     public void oclBtnOne(View view) {
-        tvIn.setText("1");
+        tvIn.setText(tvIn.getText() + "1");
+    }
+    public void oclBtnTwo(View view) {
+        tvIn.setText(tvIn.getText() + "2");
+    }
+    public void oclBtnThree(View view) {
+        tvIn.setText(tvIn.getText() + "3");
+    }
+    public void oclBtnFore(View view) {
+        tvIn.setText(tvIn.getText() + "4");
+    }
+    public void oclBtnFive(View view) {
+        tvIn.setText(tvIn.getText() + "5");
+    }
+    public void oclBtnSix(View view) {
+        tvIn.setText(tvIn.getText() + "6");
+    }
+    public void oclBtnSeven(View view) {
+        tvIn.setText(tvIn.getText() + "7");
+    }
+    public void oclBtnEight(View view) {
+        tvIn.setText(tvIn.getText() + "8");
+    }
+    public void oclBtnNine(View view) {
+        tvIn.setText(tvIn.getText() + "9");
+    }
+    public void oclBtnZero(View view) {
+        tvIn.setText(tvIn.getText() + "0");
     }
 
-    public void testCalc(){
+    @SuppressLint("SetTextI18n")
+    public void oclBtnPlus(View view) {
+        String temp  = new String ((String)tvIn.getText());
+        temp = temp.substring(temp.length() - 1);
+        if(!temp.equals("+")) tvIn.setText(tvIn.getText() + "+");
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void oclBtnClear(View view) {
+        String temp  = new String ((String)tvIn.getText());
+        temp = temp.substring(temp.length() - 1);
+        if(!temp.equals("+")) tvIn.setText(tvIn.getText() + "+");
+    }
+
+    @SuppressLint("SetTextI18n")
+    /*public void oclBtnResult(View view) {
+        Expression expression = new Expression((String)tvIn.getText());
+        CharSequence answer = (CharSequence) expression.eval();
+        tvOut.setText(answer);
+    }*/
+
+    public void oclBtnDelete (View view) {
 
     }
+
 }
 
